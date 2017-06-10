ed6a4b5670a0dc221afe877dc8842c9c406bb8c9
Scott Abraham <sabraham625@gmail.com>
2017-06-10 10:44:32 -0500

    ESLint and CLI init

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
-console.log('Hello world!');
+
+const program = require('commander');
+
+function main(file) {
+  console.log('File: %s Overwrite(Y/N): %s', file, program.overwrite);
+}
+
+program.arguments('<file>');
+program.option('-w --overwrite', 'overwrite existing tests');
+
+program.action(main);
+
+program.parse(process.argv);
+
